e07c30e0842fbe05632ed7dfdc9b6f6225779f79
dnaf <definitelynotafurry@gmail.com>
2016-08-29 03:36:09 -0400

    Remove needless developer comment in summon command

--- src/commands/summon.js
+++ src/commands/summon.js
@@ -9,8 +9,7 @@ module.exports = {
 					queue: [],
 					server: message.author.voiceChannel.server
 				};
-				return stanley.reply(message, "Joined voice channel " + connection.voiceChannel.name)
-				//return connection.playFile("https://upload.wikimedia.org/wikipedia/en/d/d0/Rick_Astley_-_Never_Gonna_Give_You_Up.ogg");
+				return stanley.reply(message, "Joined voice channel " + connection.voiceChannel.name);
 			});
 		}
 	}
