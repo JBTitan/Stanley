d1aff578f3646a264b3611452d6ea10d6d791441
dnaf <definitelynotafurry@gmail.com>
2016-08-31 10:44:26 -0400

    Add descriptive replies to !tag commands
    Closes #10 and closes #11

--- src/commands/tag.js
+++ src/commands/tag.js
@@ -19,18 +19,22 @@ module.exports = {
 				// Send tag args[0]
 				return fs.readFileAsync("tags/" + message.server.id + "/" + args[0]).then((data) => {
 					return message.reply(data);
+				}).catch((err) => {
+					return message.reply("That tag could not be found.");
 				});
 			}
 			if (args.length > 1) {
 				// Set tag args[0] to args[1...]
 				let tag = args.shift();
 				return mkdirp("tags/" + message.server.id).then(() => {
-					return fs.writeFileAsync("tags/" + message.server.id + "/" + tag, args.join(" "));
-
+					return fs.writeFileAsync("tags/" + message.server.id + "/" + tag, args.join(" ")).then(() => {
+						return message.reply("Your tag has been saved.");
+					});
 				}).catch((err) => {
-					return fs.writeFileAsync("tags/" + message.server.id + "/" + tag, args.join(" "));
+					return fs.writeFileAsync("tags/" + message.server.id + "/" + tag, args.join(" ")).then(() => {
+						return message.reply("Your tag has been saved.");
+					});
 				});
-				//return message.reply("Not yet implemented.");
 			}
 		} else {
 			return new Promise((resolve, reject) => {resolve();});
